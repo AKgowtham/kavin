587b14fe27afee16d87cc2e4c98c99cca70da6f0
AKgowtham <36406445+AKgowtham@users.noreply.github.com>
2018-05-24 11:35:03 +0530

    Create h1.py

---
+++ h1.py
@@ -0,0 +1,19 @@
+n=int(input("enter the values"))
+a=[]
+b=[]
+z=" "
+for i in range(n):
+  c=input('enter the value')
+  a.append(c)
+for i in a:
+  count=a.count(i)
+  #print(count)
+  if count>=2:
+    b.append(i)
+if b==[]:
+  print('unique')
+else:
+  b.sort()
+  s=set(b)
+  z=' '.join(s)
+  print(z)
