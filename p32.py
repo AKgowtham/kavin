807b9240e63be43b6d5ad22a35fc0863fb9442df
AKgowtham <36406445+AKgowtham@users.noreply.github.com>
2018-05-24 16:30:49 +0530

    Create p32.py

---
+++ p32.py
@@ -0,0 +1,10 @@
+a=int(input("enter"))
+d=int(input("enter the checksum"))
+b=[]
+for i in range (0,a):
+  c=input("enter the size")
+  b.append(c)
+if d in b :
+  print("yes")
+else:
+  print("no")
